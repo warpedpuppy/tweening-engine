d3c8f0c223d17b13d55e9a466ad719279fafe4a5
ted <ted@warpedpuppy.com>
2020-09-14 14:38:51 -0400

    added multiple concurrent tweens

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import tweening from './tweening/tweening.js';
-
+import utils from './tweening/utils.js';
 (function() {
 
         const app = new PIXI.Application({
@@ -7,25 +7,24 @@ import tweening from './tweening/tweening.js';
         });
         document.getElementById('canvas-div').appendChild(app.view);
         document.getElementById('start-animation').addEventListener('click', startAction)
-
+        document.getElementById('state').addEventListener('change', changeRadioButton)
 
         // create character
         const texture = PIXI.Texture.from('./bmps/character.png');
-        const character = new PIXI.Sprite(texture);
-        character.anchor.set(0.5);
-        character.width = 154; //NB: THIS HAS BEEN HARD CODED BECAUSE IF ASSET ISN'T LOADED VIA A LOADER, THE WIDTH WILL BE "1" REFELCTING THE %
-        character.startX = character.width / 2;
-        character.endX = app.screen.width - character.startX;
-        character.x = character.startX;
-        character.y = app.screen.height / 2;
-        
-        app.stage.addChild(character);
+        //NB: characterWidth HAS BEEN HARD CODED BECAUSE IF ASSET ISN'T LOADED VIA A LOADER, THE WIDTH WILL BE "1" REFELCTING THE %
+        let state = "single", characterWidth = 154, characters = [], counter = 0;
+        setUpSingle();
+
+        function createCharacter (obj) {
+            const character = new PIXI.Sprite(texture);
+            character.anchor.set(0.5);
+            character.scale.set(obj.scale);
+            character.x = obj.startX;
+            character.y = obj.startY;
+            character.props = obj;
+            return character;
+        }
        
-
-
-       
-        
-
         //make text field
         const basicText = new PIXI.Text();
         basicText.anchor.set(0.5);
@@ -34,13 +33,73 @@ import tweening from './tweening/tweening.js';
 
         app.stage.addChild(basicText);
 
+        function setUpSingle () {
+            app.stage.removeChildren();
+            let obj = {
+                startX: characterWidth / 2,
+                startY: app.screen.height / 2,
+                endX: app.screen.width - (characterWidth / 2),
+                endY: app.screen.height / 2,
+                duration: 2,
+                scale: 1
+            }
+            let character = createCharacter(obj);
+            characters = [character];
+            app.stage.addChild(character);
+        }
+        function setUpMultiple () {
+            characters = [];
+            app.stage.removeChildren();
+            for (let i = 0; i < 100; i ++ ) {
+                let obj = {
+                    startX: utils.randomNumberBetween(0, app.screen.width),
+                    startY: utils.randomNumberBetween(0, app.screen.height),
+                    endX: utils.randomNumberBetween(0, app.screen.width),
+                    endY: utils.randomNumberBetween(0, app.screen.height),
+                    scale: utils.randomNumberBetween(0, 1),
+                    duration: utils.randomNumberBetween(.04, 2)
+                }
+                let character = createCharacter(obj);
+                characters.push(character);
+                app.stage.addChild(character);
+            }
+        }
         function startAction () {
+            tweening.killAll();
+            counter = 0;
+            document.getElementById('start-animation').disabled = true;
             basicText.text = "";
-            tweening.tween(character, 2, {x: [character.startX, character.endX]}, actionComplete,'easeOutBounce');
+            for (let i = 0; i < characters.length; i++) {
+                let character = characters[i];
+                 tweening.tween(character, character.props.duration, {
+                x: [character.props.startX, character.props.endX], 
+                y: [character.props.startY, character.props.endY]
+            }, actionComplete,'easeOutBounce');
+            }
+           
         }
 
         function actionComplete () {
             basicText.text = "action complete!";
+            if (state === 'single') {
+                document.getElementById('start-animation').disabled = false;
+            } else {
+                counter ++;
+                if (counter === 100) {
+                    document.getElementById('start-animation').disabled = false;
+                }
+            }
+        }
+
+        function changeRadioButton (e) {
+            if (e.target.value === 'single') {
+                state = 'single';
+                setUpSingle();
+            } else {
+                state = 'multiple';
+                setUpMultiple();
+            }
+
         }
 
 
