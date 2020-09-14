dd2e930e3a188912b6b2b3163f1c760f6568baf7
ted <ted@warpedpuppy.com>
2020-09-14 13:23:54 -0400

    styled html"

--- index.js
+++ index.js
@@ -3,20 +3,45 @@ import tweening from './tweening/tweening.js';
 (function() {
 
         const app = new PIXI.Application({
-            width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+            width: 800, height: 600, transparent: true, resolution: window.devicePixelRatio || 1,
         });
-        document.body.appendChild(app.view);
+        document.getElementById('canvas-div').appendChild(app.view);
+        document.getElementById('start-animation').addEventListener('click', startAction)
 
+
+        // create character
         const texture = PIXI.Texture.from('./bmps/character.png');
         const character = new PIXI.Sprite(texture);
         character.anchor.set(0.5);
-        character.x = 100;
-        character.y = 100;
-        app.stage.addChild(character);
         character.width = 154; //NB: THIS HAS BEEN HARD CODED BECAUSE IF ASSET ISN'T LOADED VIA A LOADER, THE WIDTH WILL BE "1" REFELCTING THE %
-        let startX = character.width / 2;
-        let endX = app.screen.width - startX;
-        tweening.tween(character, 2, {x: [startX, endX]}, undefined,'easeOutBounce');
+        character.startX = character.width / 2;
+        character.endX = app.screen.width - character.startX;
+        character.x = character.startX;
+        character.y = app.screen.height / 2;
+        
+        app.stage.addChild(character);
+       
+
+
+       
+        
+
+        //make text field
+        const basicText = new PIXI.Text();
+        basicText.anchor.set(0.5);
+        basicText.x = app.screen.width / 2;
+        basicText.y = (app.screen.height - 100);
+
+        app.stage.addChild(basicText);
+
+        function startAction () {
+            basicText.text = "";
+            tweening.tween(character, 2, {x: [character.startX, character.endX]}, actionComplete,'easeOutBounce');
+        }
+
+        function actionComplete () {
+            basicText.text = "action complete!";
+        }
 
 
         // Listen for animate update
